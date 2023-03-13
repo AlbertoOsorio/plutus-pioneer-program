c1d19eb2d3cda8a8e6d1b3e9e23ec8c4b227bb68
CorUrat <albertox1320@gmail.com>
2023-03-13 13:32:25 +0000

    Weeek2 homework

--- code/Week02/homework/Homework1.hs
+++ code/Week02/homework/Homework1.hs
@@ -8,6 +8,8 @@ module Homework1 where
 import Plutus.V2.Ledger.Api qualified as PlutusV2
 import PlutusTx
 import PlutusTx.Prelude 
+import Prelude (IO)
+import Utils (writePlutusFile)
 import Utils (wrap)
 
 ---------------------------------------------------------------------------------------------------
@@ -16,10 +18,13 @@ import Utils (wrap)
 {-# INLINABLE mkValidator #-}
 -- This should validate if and only if the two Booleans in the redeemer are True!
 mkValidator :: () -> (Bool, Bool) -> PlutusV2.ScriptContext -> Bool
-mkValidator _ _ _ = False
+mkValidator _ (x, y) _ = x == y
 
 wrappedVal :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 wrappedVal = wrap mkValidator
 
 validator :: PlutusV2.Validator
 validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| wrappedVal ||])
+
+saveVal :: IO ()
+saveVal = writePlutusFile "./assets/hw1.plutus" validator
--- code/Week02/homework/Homework2.hs
+++ code/Week02/homework/Homework2.hs
@@ -11,8 +11,10 @@ module Homework2 where
 import Plutus.V2.Ledger.Api qualified as PlutusV2
 import PlutusTx
 import PlutusTx.Prelude 
+import Prelude (IO)
+import Utils (writePlutusFile)
 import Prelude (undefined)
---import Utils (wrap)
+import Utils (wrap)
 
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
@@ -27,10 +29,13 @@ PlutusTx.unstableMakeIsData ''MyRedeemer
 {-# INLINABLE mkValidator #-}
 -- Create a validator that unlocks the funds if MyRedemeer's flags are different
 mkValidator :: () -> MyRedeemer -> PlutusV2.ScriptContext -> Bool
-mkValidator = undefined
+mkValidator _ (MyRedeemer x y) _ = x /= y
 
 wrappedVal :: BuiltinData -> BuiltinData -> BuiltinData -> ()
-wrappedVal = undefined
+wrappedVal = wrap mkValidator
 
 validator :: PlutusV2.Validator
-validator = undefined
+validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| wrappedVal ||])
+
+saveVal :: IO ()
+saveVal = writePlutusFile "./assets/hw2.plutus" validator
